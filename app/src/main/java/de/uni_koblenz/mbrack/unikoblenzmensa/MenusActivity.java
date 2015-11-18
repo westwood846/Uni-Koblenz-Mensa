d68021a880a6ece35751e010b53b3b2421dde1b8
turbopope <PapstDonB@Googlemail.com>
2015-11-18 15:01:21 +0100

    Rename MenuActivity to MenusActivity
    In future, MenusActivity will hold many MenuFragments with one
    fragment for each day's menu

--- app/src/main/java/de/uni_koblenz/mbrack/unikoblenzmensa/MenusActivity.java
+++ app/src/main/java/de/uni_koblenz/mbrack/unikoblenzmensa/MenusActivity.java
@@ -7,7 +7,7 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenusActivity extends AppCompatActivity {
     private ListView listView;
     private RecyclerView.LayoutManager layoutManager;
 
@@ -16,7 +16,7 @@ public class MenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_menus);
 
         listView = (ListView) findViewById(R.id.menu_item_list);
 
