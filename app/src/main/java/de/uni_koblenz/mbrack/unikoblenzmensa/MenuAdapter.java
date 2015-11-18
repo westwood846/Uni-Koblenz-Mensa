1ff4c3d295eb9b62dc8300c59f5651fc228a8a56
turbopope <PapstDonB@Googlemail.com>
2015-11-18 10:53:14 +0100

    Implement MenuAdapter with some static test data and TextViews

--- app/src/main/java/de/uni_koblenz/mbrack/unikoblenzmensa/MenuAdapter.java
+++ app/src/main/java/de/uni_koblenz/mbrack/unikoblenzmensa/MenuAdapter.java
@@ -1,21 +1,36 @@
 package de.uni_koblenz.mbrack.unikoblenzmensa;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MenuAdapter extends RecyclerView.Adapter {
+    private String[] items = new String[]{"Alle", "Meine", "Entchen"};
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+        public ViewHolder(TextView v) {
+            super(v);
+            textView = v;
+        }
+    }
+
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        TextView newView = ((TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.menu_item_text_view, parent, false));
+        ViewHolder newViewHolder = new ViewHolder(newView);
+        return newViewHolder;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        ((ViewHolder) holder).textView.setText(items[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.length;
     }
 }
