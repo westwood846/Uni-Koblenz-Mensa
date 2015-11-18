6a1f9dc0c553a5773c2a420ac7cc2e98c7411d26
turbopope <PapstDonB@Googlemail.com>
2015-11-18 10:17:46 +0100

    Create empty MenuAdapter

---
+++ app/src/main/java/de/uni_koblenz/mbrack/unikoblenzmensa/MenuAdapter.java
@@ -0,0 +1,21 @@
+package de.uni_koblenz.mbrack.unikoblenzmensa;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class MenuAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
