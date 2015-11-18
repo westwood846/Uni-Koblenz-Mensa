68f0dd8c668f3a3fded8f3c34ce8c6c5ccfb6995
turbopope <PapstDonB@Googlemail.com>
2015-11-18 10:16:49 +0100

    Initialize menu Recycler View

--- app/src/main/java/de/uni_koblenz/mbrack/unikoblenzmensa/MenuActivity.java
+++ app/src/main/java/de/uni_koblenz/mbrack/unikoblenzmensa/MenuActivity.java
@@ -2,12 +2,25 @@ package de.uni_koblenz.mbrack.unikoblenzmensa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MenuActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter menuAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        recyclerView = (RecyclerView) findViewById(R.id.activity_menu_recycler_view);
+
+        menuAdapter = new MenuAdapter();
+        recyclerView.setAdapter(menuAdapter);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
