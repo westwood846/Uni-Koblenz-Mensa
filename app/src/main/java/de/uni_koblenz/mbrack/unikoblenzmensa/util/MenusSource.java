e00825dd73116a217ad472b0e00484b51dfcc8af
turbopope <PapstDonB@Googlemail.com>
2015-11-19 14:59:19 +0100

    Create interface for Menus sources

---
+++ app/src/main/java/de/uni_koblenz/mbrack/unikoblenzmensa/util/MenusSource.java
@@ -0,0 +1,9 @@
+package de.uni_koblenz.mbrack.unikoblenzmensa.util;
+
+import java.util.List;
+
+import de.uni_koblenz.mbrack.unikoblenzmensa.entity.Menu;
+
+public interface MenusSource {
+    List<Menu> getMenus();
+}
