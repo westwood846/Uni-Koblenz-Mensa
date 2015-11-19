879955076811ae2e6fe749bd6b7e4cf3045db1fe
turbopope <PapstDonB@Googlemail.com>
2015-11-19 16:17:13 +0100

    Make MenusNitAvailableException unchecked

--- app/src/main/java/de/uni_koblenz/mbrack/unikoblenzmensa/fetch/MenusNotAvailableException.java
+++ app/src/main/java/de/uni_koblenz/mbrack/unikoblenzmensa/fetch/MenusNotAvailableException.java
@@ -1,4 +1,4 @@
 package de.uni_koblenz.mbrack.unikoblenzmensa.fetch;
 
-public class MenusNotAvailableException extends Exception {
+public class MenusNotAvailableException extends RuntimeException {
 }
